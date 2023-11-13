436b1f57bd6b1de2d38d17f4577012f21fd9eb38
darkmanneke <darkmanneke@users.noreply.github.com>
2023-11-13 10:16:09 +0100

    upgrade rdap-server lib from 3.0.1 to 4.0.0, upgrade spring boot from 2.7.16 to 3.0.12

--- src/main/java/be/dnsbelgium/rdap/sample/springbootjar/controlleradvice/SampleControllerAdvice.java
+++ src/main/java/be/dnsbelgium/rdap/sample/springbootjar/controlleradvice/SampleControllerAdvice.java
@@ -1,15 +1,11 @@
 package be.dnsbelgium.rdap.sample.springbootjar.controlleradvice;
 
 import be.dnsbelgium.rdap.core.RDAPError;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
